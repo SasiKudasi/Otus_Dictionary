a2fc48596f0d4da67d610470c3b919de2ec75c77
SasiKudasi <sasha.mittal@yandex.ru>
2024-03-22 18:11:50 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Otus_Dictionary
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
